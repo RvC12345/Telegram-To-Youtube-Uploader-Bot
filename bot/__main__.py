8cf650941ee0ccce55b956e931b08cdf82177938
RvC12345 <ravigimhan31@gmail.com>
2024-11-22 04:28:40 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -5,9 +5,5 @@ from .config import Config
 
 
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.DEBUG if Config.DEBUG else logging.INFO)
-    logging.getLogger("pyrogram").setLevel(
-        logging.INFO if Config.DEBUG else logging.WARNING
-    )
 
     UtubeBot().run()
